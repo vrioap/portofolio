0a600e29aaa800593ecab369c154318ae5cc2a2f
Vrizzo <ijonggg5@gmail.com>
2024-10-22 08:30:41 +0700

    Add files via upload
    DLL

---
+++ process.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    echo "<h1>Message Received!</h1>";
+    echo "<p><strong>Name:</strong> $name</p>";
+    echo "<p><strong>Email:</strong> $email</p>";
+    echo "<p><strong>Message:</strong> $message</p>";
+} else {
+    echo "Invalid request method.";
+}
+?>
